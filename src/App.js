26f93749b8cf9efb8b4ac5065d4b86b972cd9195
Akash Kumar Seth <kumarakashseth007@gmail.com>
2019-02-06 04:08:03 +0530

    Main header part UI completed.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Routes from "./components/routes/routes";
 
-
 class App extends Component {
   render() {
     return (
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Input } from 'antd';
+import RecipeList from "./recipeList";
+
+const Search = Input.Search;
 
 const Main = (props) => {
   return (
-    <div>Main Page</div>
+    <div>
+      <div id="title" className="newFont">Search For Recipe With <span>Food2Fork</span></div>
+      <div id="searchTitle">Type Recipes Separated By Comma</div>
+      <div id="searchBar">
+      <Search
+        placeholder="input search text"
+        onSearch={value => console.log(value)}
+        enterButton size="large"
+      />
+      </div>
+      <div id="listTitle" className="newFont">recipe list</div>
+      <RecipeList recipes={props.recipes}/>
+    </div>
   )
 };
 
